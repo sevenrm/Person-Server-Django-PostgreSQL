1012288a0eae88402c318fba0eaa44fbaddec213
Seven <sevenrm.dev109@gmail.com>
2023-11-12 19:08:15 +0200

    Django and PostgreSQL person project

---
+++ person/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from person.models import PersonModel
+
+class PersonSerializer(ModelSerializer):
+    class Meta:
+        model = PersonModel
+        fields = ['id', 'name', 'address', 'phone']
